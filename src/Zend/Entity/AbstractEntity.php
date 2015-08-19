4f573802c3e819b9620ea7838d3337472786420b
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2015-08-19 21:33:16 +0100

    start

---
+++ src/Zend/Entity/AbstractEntity.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: piotr
+ * Date: 8/19/15
+ * Time: 9:26 PM
+ */
+class AbstractEntity
+{
+    public function __construct($data = array())
+    {
+        if ($data != null) {
+            foreach ($data as $key => $val) {
+
+            }
+        }
+    }
+}
