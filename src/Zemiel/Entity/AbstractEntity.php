f2f9293a9399b72f7f799e2fa80ff22917a9056d
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2015-08-19 23:08:10 +0100

    comments

--- src/Zemiel/Entity/AbstractEntity.php
+++ src/Zemiel/Entity/AbstractEntity.php
@@ -13,6 +13,11 @@ class AbstractEntity
     private $entityData = [];
     private $entityProperties = [];
 
+    /**
+     * @param array $data
+     *
+     * construct function
+     */
     public function __construct($data = [])
     {
 
@@ -29,10 +34,15 @@ class AbstractEntity
         }
     }
 
+    /**
+     * @return array
+     *
+     * getting class properties
+     */
     public function getProperties()
     {
         $reflect = new ReflectionClass($this);
-        $properties   = $reflect->getProperties(ReflectionProperty::IS_PROTECTED);
+        $properties = $reflect->getProperties(ReflectionProperty::IS_PROTECTED);
 
         foreach ($properties as $property) {
             $this->entityProperties[] = $property->getName();
