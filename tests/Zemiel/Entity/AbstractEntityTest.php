377f878d154624756db8c3af5f292690a0ddcd90
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2015-08-21 23:46:07 +0100

    test

--- tests/Zemiel/Entity/AbstractEntityTest.php
+++ tests/Zemiel/Entity/AbstractEntityTest.php
@@ -30,13 +30,13 @@ class AbstractEntityTest extends \PHPUnit_Framework_TestCase
     public function testCanBeNegated()
     {
         $data = ['name' => 'Piotr', 'type' => 'man', 'color' => 'white', 'test' => 'error'];
-        // Arrange
+
         $a = new TestClass();
 
         $a->setEntityData($data);
 
         $b = ['name' => 'Piotr', 'type' => 'man', 'color' => 'white'];
-        // Assert
+
         $this->assertEquals($b, $a->getEntityData());
     }
 }
