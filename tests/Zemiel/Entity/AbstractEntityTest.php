74579d8e628cd69c77f9620781db4e0f4d6200a8
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2015-08-23 22:31:42 +0100

    terts

--- tests/Zemiel/Entity/AbstractEntityTest.php
+++ tests/Zemiel/Entity/AbstractEntityTest.php
@@ -46,7 +46,7 @@ class TestClass extends AbstractEntity
 class ExceptionTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @expectedException Exception
+     * @expectedException Data must be array.
      */
     public function testException()
     {
