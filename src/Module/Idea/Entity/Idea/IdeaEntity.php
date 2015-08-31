76a85f6b6c2d377767c833d6cfa9461b32f9b094
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2015-08-31 21:48:03 +0100

    changes in IdeaEntity class

--- src/Module/Idea/Entity/Idea/IdeaEntity.php
+++ src/Module/Idea/Entity/Idea/IdeaEntity.php
@@ -47,7 +47,7 @@ class IdeaEntity
      */
     public function setTitle($title)
     {
-        if (is_string($title)) {
+        if (!(is_string($title))) {
             throw new \Exception('Must be string.');
         }
         $this->title = $title;
@@ -72,7 +72,7 @@ class IdeaEntity
      */
     public function setContent($content)
     {
-        if (is_string($content)) {
+        if (!(is_string($content))) {
             throw new \Exception('Must be string.');
         }
         $this->content = $content;
@@ -97,7 +97,7 @@ class IdeaEntity
      */
     public function setUserId($userId)
     {
-        if (is_int($userId)) {
+        if (!(is_int($userId))) {
             throw new \Exception('Must be integer.');
         }
         $this->userId = $userId;
