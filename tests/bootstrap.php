461fd560cb4718f7f880bf4dd77e2e24a7a102da
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2015-09-15 23:54:11 +0100

    updated bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,6 @@ if (function_exists('date_default_timezone_set') && function_exists('date_defaul
     date_default_timezone_set(@date_default_timezone_get());
 }
 
-require 'vendor/autoload.php';
+chdir(dirname(__DIR__));
+
+require __DIR__ . '../vendor/autoload.php';
