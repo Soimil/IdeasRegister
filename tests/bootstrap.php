c6898f2232f045966b5470988a930def376b3a97
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2015-09-16 23:31:27 +0100

    bootsrap update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,9 +7,9 @@ if (function_exists('date_default_timezone_set') && function_exists('date_defaul
 
 //chdir(__DIR__);
 
-//require __DIR__ . '/../vendor/autoload.php';
-if (!@include __DIR__ . '/../vendor/autoload.php') {
-    die('You must set up the project dependencies, run the following commands:
-        wget http://getcomposer.org/composer.phar
-        php composer.phar install');
-}
+require __DIR__ . '/../vendor/autoload.php';
+//if (!@include __DIR__ . '/../vendor/autoload.php') {
+//    die('You must set up the project dependencies, run the following commands:
+//        wget http://getcomposer.org/composer.phar
+//        php composer.phar install');
+//}
