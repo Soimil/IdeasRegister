5a8e4bdbc889eb9635e23d65c87de8e29a34bfe9
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2015-09-17 22:19:47 +0100

    InvalidArgumentException

--- src/Zemiel/Mapper/AbstractMapper.php
+++ src/Zemiel/Mapper/AbstractMapper.php
@@ -20,7 +20,7 @@ abstract class AbstractMapper
      */
     public function setName($name)
     {
-        if (!(is_string($name)) || preg_match('/\s/', $name) > 0 || strlen($name) === 0) {
+        if (!(is_string($name)) || preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]//\s/', $name) > 0 || strlen($name) === 0) {
             throw new \InvalidArgumentException('Mapper name can\'t by empty and must be string.');
         }
 
