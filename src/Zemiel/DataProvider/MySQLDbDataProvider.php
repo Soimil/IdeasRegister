2706043eb196ca2c38cee66378b6b0b94437226e
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2016-01-08 22:10:33 +0000

    update

--- src/Zemiel/DataProvider/MySQLDbDataProvider.php
+++ src/Zemiel/DataProvider/MySQLDbDataProvider.php
@@ -8,39 +8,175 @@
 
 namespace Zemiel\DataProvider;
 
+use MongoDB\Driver\Manager;
+use MongoDB\Driver\BulkWrite;
+use MongoDB\Driver\Query;
+use MongoDB\Driver\ReadPreference;
+use MongoDB\Driver\WriteConcern;
+use MongoDB\Collection;
+
 class MySQLDbDataProvider
 {
     /**
      * @var \MongoDB
      */
-    protected static $db;
+    protected static $manager;
+    protected static $writeConcern;
+    protected static $readPreference;
+    protected static $bulk;
+    protected static $query;
 
-    /**
-     * @var array
-     */
-    protected static $options = [
-        'journal' => true,
-        'readPreference' => 'secondary',
+    protected static $uri = 'mongodb://localhost:27017';
+    protected static $preference = [
+        [],
+        [],
+        [],
     ];
 
-    protected static $uri = 'mongodb://localhost/';
-
     /**
-     * MySQLDbDataProvider constructor.
+     * MySQLDbDataProvider constructor
+     *
+     * @protected
      */
     protected function __construct()
     {
-        $m = new \MongoClient(self::$uri, self::$options);
-        self::$db = $m->ideas_register;
-        return self::$db;
+        self::setManager();
+        self::setWriteConcern();
+        self::setReadPreference();
+        self::setBulk();
+        self::setQuery();
     }
 
     /**
+     * setManager
+     *
+     * setting MongoDB Driver Manager
+     *
+     * @protected
+     */
+    protected function setManager()
+    {
+        self::$manager = new Manager(self::$uri);
+    }
+
+    /**
+     * setWriteConcern
+     *
+     * setting MongoDB WriteConcern
+     *
+     * @protected
+     */
+    protected function setWriteConcern()
+    {
+        self::$writeConcern = new WriteConcern(WriteConcern::MAJORITY, 1000);
+    }
+
+    /**
+     * setReadPreference
+     *
+     * setting MongoDB ReadPreference
+     *
+     * @protected
+     */
+    protected function setReadPreference()
+    {
+        self::$readPreference = new ReadPreference(ReadPreference::RP_SECONDARY_PREFERRED, self::$preference);
+    }
+
+    /**
+     * setBulk
+     *
+     * setting MongoDB BulkWrite
+     *
+     * @protected
+     */
+    protected function setBulk()
+    {
+        self::$bulk = new BulkWrite();
+    }
+
+    /**
+     * setQuery
+     *
+     * setting MongoDB Query
+     *
+     * @protected
+     */
+    protected function setQuery()
+    {
+        self::$query = new Query([]);
+    }
+
+    /**
+     * getInstance
+     *
+     * getting Instance This Class
+     *
+     * @static
      * @return \MongoDB|MySQLDbDataProvider
      */
     public static function getInstance()
     {
-        return self::$db ? self::$db : new self();
+        return new self();
     }
 
+    /**
+     * getManager
+     *
+     * getting MongoDB Manager
+     *
+     * @return \MongoDB
+     */
+    public function getManager()
+    {
+        return self::$manager;
+    }
+
+    /**
+     * getWriteConcern
+     *
+     * getting WriteConcern
+     *
+     * @return mixed
+     */
+    public function getWriteConcern()
+    {
+        return self::$writeConcern;
+    }
+
+    /**
+     * getReadPreference
+     *
+     * getting ReadPreference
+     *
+     * @return mixed
+     */
+    public function getReadPreference()
+    {
+        return self::$readPreference;
+    }
+
+    /**
+     * getBulk
+     *
+     * getting Bulk
+     *
+     * @return mixed
+     */
+    public function getBulk()
+    {
+        return self::$bulk;
+    }
+
+    /**
+     * getQuery
+     *
+     * getting Query
+     *
+     * @return mixed
+     */
+    public function getQuery()
+    {
+        return self::$query;
+    }
 }
--- tests/Zemiel/DataProvider/MySQLDbDataProviderTest.php
+++ tests/Zemiel/DataProvider/MySQLDbDataProviderTest.php
@@ -8,15 +8,99 @@
 
 namespace tests\Zemiel\DataProvider;
 
+use MongoDB\Driver\Exception\Exception;
 use Zemiel\DataProvider\MySQLDbDataProvider as MySql;
 
 class MySQLDbDataProvider extends \PHPUnit_Framework_TestCase
 {
     /**
-     *
+     * testCreate
      */
-    public function testConnectToDataBase()
+    public function testCreate()
     {
-        $db = MySql::getInstance();
+        $document = ["hello" => "world"];
+
+        $mongoDb = MySql::getInstance();
+        $manager = $mongoDb->getManager();
+        $wc = $mongoDb->getWriteConcern();
+        $bulk = $mongoDb->getBulk();
+
+        $bulk->insert($document);
+
+        try {
+            $result = $manager->executeBulkWrite("db.collection", $bulk, $wc);
+            var_dump($result);
+        } catch (Exception $e) {
+            echo $e->getMessage(), "\n";
+        }
+    }
+
+    /**
+     * testGetQuery
+     */
+    public function testGetQuery()
+    {
+        $mongoDb = MySql::getInstance();
+        $manager = $mongoDb->getManager();
+
+        $query = $mongoDb->getQuery();
+        $rp = $mongoDb->getReadPreference();
+
+        try {
+            $cursor = $manager->executeQuery("db.collection", $query, $rp);
+
+            foreach ($cursor as $document) {
+                var_dump($document);
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage(), "\n";
+        }
+
+    }
+
+    /**
+     * testUpdate
+     */
+    public function testUpdate()
+    {
+        $filter = ["hello" => "world"];
+        $update = ['$set' => ["hello" => "wonderful world"]];
+
+        $options = ["limit" => 1, "upsert" => false];
+
+        $mongoDb = MySql::getInstance();
+        $manager = $mongoDb->getManager();
+        $wc = $mongoDb->getWriteConcern();
+        $bulk = $mongoDb->getBulk();
+
+        $bulk->update($filter, $update, $options);
+
+        try {
+            $result = $manager->executeBulkWrite("db.collection", $bulk, $wc);
+            var_dump($result);
+        } catch (Exception $e) {
+            echo $e->getMessage(), "\n";
+        }
+    }
+
+    public function testDelete()
+    {
+        $filter = ["hello" => "world"];
+        $options = ["limit" => 1];
+
+        $mongoDb = MySql::getInstance();
+        $manager = $mongoDb->getManager();
+        $wc = $mongoDb->getWriteConcern();
+        $bulk = $mongoDb->getBulk();
+
+        $bulk->delete($filter, $options);
+
+        try {
+            $result = $manager->executeBulkWrite("db.collection", $bulk, $wc);
+            var_dump($result);
+        } catch (Exception $e) {
+            echo $e->getMessage(), "\n";
+        }
+
     }
 }
